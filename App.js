ae33a7231236aaa04f7f42f6500e67df8aacc91e
Marcelo Santos <rocha.mer21@gmail.com>
2021-03-13 22:25:10 -0300

    start in study

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+const App = () => {
+  return (
+    <View style={backgroundStyle}>
+      <Text>Oieeeeeeeeeeee</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default App
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# React Navigation 5 -> React Native
+
+Estudo do React Navigation
+-- StackNavigator
+-- TabNavigator
+-- DrawerNavigator
